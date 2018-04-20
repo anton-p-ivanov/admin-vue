a657ff2b24d8af305b8f755ee634ce28cc6a27cf
Anton Ivanov <yapi68@ya.ru>
2018-04-20 21:06:47 +0300

    After 2018-04-20 21:00

--- src/mocks/app.sidebar.js
+++ src/mocks/app.sidebar.js
@@ -12,16 +12,27 @@ export const APP_SIDEBAR = [
     },
     {
         text: 'Storage',
-        // icon: 'mdi-folder-multiple',
         icon: 'mdi-file-document-box',
         url: '#'
     },
     {
         text: 'Users',
         icon: 'mdi-account-multiple',
+        items: [
+            { text: 'It seamlessly merges', url: '#' },
+            { text: 'JSON Schema Normalizer is a small library built on top of ajv and normalizr', url: '#' },
+            { divider: true },
+            { text: 'Normalize your data by JSON Schema', url: '#', disabled: true }
+        ]
     },
     {
         text: 'Accounts',
         icon: 'mdi-briefcase',
+        items: [
+            { text: 'It seamlessly merges', url: '#' },
+            { text: 'JSON Schema Normalizer is a small library built on top of ajv and normalizr', url: '#' },
+            { divider: true },
+            { text: 'Normalize your data by JSON Schema', url: '#', disabled: true }
+        ]
     }
 ];
