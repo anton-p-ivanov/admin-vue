ab327f50672f50cab24a7090b450c7569d802db2
Anton Ivanov <yapi68@ya.ru>
2018-04-20 10:12:20 +0300

    New shared component: Tabs

---
+++ src/components/shared/Tabs/Tabs.js
@@ -0,0 +1,97 @@
+// noinspection JSUnusedGlobalSymbols
+export default {
+    name: 'Tabs',
+    props: {
+        items: {
+            type: Array
+        }
+    },
+    data () {
+        return {
+            toggle: null,
+            target: null
+        }
+    },
+    methods: {
+        tabID (index) {
+            return this.items[index].hasOwnProperty('id') ? this.items[index].id : 'tab-' + index;
+        },
+
+        toggleTab (index) {
+            index = index || 0;
+            this.$el.querySelectorAll('[data-toggle="tab"]')[index].click();
+        },
+
+        init () {
+            // Set handler to listen tabs click
+            document.addEventListener('click', this.__initHandler);
+
+            // Toggle first tab
+            this.toggleTab();
+        },
+
+        show () {
+            // Make component visible
+            this.toggle.classList.add('active');
+            this.target.classList.add('visible');
+
+            // Dispatch shown.Modal custom event
+            this.target.dispatchEvent(new Event('shown.Tabs', { cancelable: true }))
+        },
+
+        hide () {
+            let other = this.$el.querySelectorAll('[data-toggle="tab"]');
+
+            Array.prototype.forEach.call(other, function (element) {
+                element.classList.remove('active');
+
+                // Dispatch hidden.Modal custom event
+                element.dispatchEvent(new Event('hidden.Tabs', { cancelable: true }))
+            });
+
+            Array.prototype.forEach.call(this.$el.querySelectorAll('.tabs-pane'), function (element) {
+                element.classList.remove('visible')
+            })
+        },
+
+        __initHandler (event) {
+            let target = event.target;
+
+            while (target && target !== document) {
+                if (target.dataset && target.dataset.toggle === 'tab') {
+                    event.preventDefault();
+
+                    this.toggle = target;
+                    if (this.toggle.classList.contains('disabled')) {
+                        return false
+                    }
+
+                    this.target = document.querySelector(this.toggle.dataset.target);
+
+                    // Hide all other tabs in current tab container
+                    this.hide();
+
+                    // Show current component
+                    return this.show()
+                }
+
+                target = target.parentNode
+            }
+        }
+    },
+    created () {
+        this.items.map((item, index) => {
+            let options = {
+                'data-toggle': 'tab',
+                'data-target': '#' + this.tabID(index)
+            };
+
+            item.options = { ...options, ...item.options };
+
+            return item;
+        });
+    },
+    mounted () {
+        this.init();
+    }
+}
---
+++ src/components/shared/Tabs/Tabs.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="tabs">
+        <ul class="tabs-nav">
+            <li v-for="(item, index) in items" :key="tabID(index)" class="tabs-nav__item">
+                <a class="tabs-nav__link" v-bind="item.options">
+                    {{ item.label }}
+                </a>
+            </li>
+        </ul>
+        <div class="tabs-content">
+            <div v-for="(item, index) in items" :key="index" :id="tabID(index)" class="tabs-pane">
+                <template v-if="item.component">
+                    <component :is="item.component"></component>
+                </template>
+                <template v-else>
+                    {{ item.content }}
+                </template>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="./Tabs.js"></script>
+<style src="./Tabs.scss" lang="scss"></style>
